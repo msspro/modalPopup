e920458f2b15db9c21e94266b102a850b6012ace
MSSPRO <rayavila@gmail.com>
2020-12-09 15:00:08 -0600

    Css y config file

---
+++ modalPopup/highslide/highslide-config.js
@@ -0,0 +1,48 @@
+hs.graphicsDir = 'https://msspro.github.io/modalPopup/modalPopup/highslide/graphics/';
+hs.outlineType = 'rounded-white';
+hs.wrapperClassName = 'draggable-header';
+
+(function() {
+	
+		const urlInfo = 'https://www.atratopago.com/';
+		const urlPago = 'https://www.atratopago.com/formulario';
+	
+		const addbtnAtratoInfo = () => {
+			var divAtratoInfo = document.getElementById("higslide-div-atrato-info");
+			if (divAtratoInfo === null){
+				return;
+			};
+			// Agregar el codigo necesario a visualizar en el control.
+		    const innerHTML = `
+						<a id='linkHref1' href="${urlInfo}" class="btn-atrato-info-link"  style="display:block" 
+							onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">
+							boton atrato info
+						</a>
+												
+		  `;
+		  divAtratoInfo.innerHTML = innerHTML;		  
+		};
+		
+		const addbtnAtratoPago = () => {
+			var divAtratoPago = document.getElementById("higslide-div-atrato-pago");
+			if (divAtratoPago === null){
+				return;
+			}
+			
+			// Agregar el codigo necesario a visualizar en el control.
+		    const innerHTML = `
+						<a id='linkHref1' href="${urlPago}"  class="btn-atrato-info-link"  style="display:block" 
+							onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">
+							boton atrato pago
+						</a>
+						
+						
+		  `;
+		  divAtratoPago.innerHTML = innerHTML;		  
+		};
+
+		addbtnAtratoInfo();
+		addbtnAtratoPago();
+
+})();
+
