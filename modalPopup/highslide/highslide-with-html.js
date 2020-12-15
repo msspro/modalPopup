cc7ffdd4c6a7fd4c039d108c4fd26ef7d5607021
MSSPRO <rayavila@gmail.com>
2020-12-15 11:28:10 -0600

    Update highslide-with-html.js
    fullxpand

--- modalPopup/highslide/highslide-with-html.js
+++ modalPopup/highslide/highslide-with-html.js
@@ -22,7 +22,7 @@ lang : {
 	previousTitle : 'Previous (arrow left)',
 	nextTitle : 'Next (arrow right)',
 	moveTitle : 'Move',
-	fullExpandText : '1:1',
+	fullExpandText : '1:2',
 	restoreTitle : 'Click to close image, click and drag to move. Use arrow keys for next and previous.'
 },
  
